d8904a02fdd2036e9ac160eaf654538f938bfd22
user.name : "Ayush <kumarayush8117@gmail.com>
2026-07-09 23:05:52 +0530

    push this on github

--- 09-useRefHook/src/App.jsx
+++ 09-useRefHook/src/App.jsx
@@ -2,24 +2,37 @@ import React, { useEffect, useRef, useState } from 'react'
 
 const App = () => {
 
-  const [count, setCount] = useState(0);
-  const prevCount = useRef();
-  
-  useEffect(() => {
-    prevCount.current = count;
-  }, [count])
+  const inputRef = useRef();
+  const [name, setName] = useState("Ayush");
+
+  const handleReset = () => {
+    setName("");
+    inputRef.current.focus();
+  }
+
+  const handleChange = () => {
+    inputRef.current.style.color = "Orange";
+  }
 
   return (
     <>
       <div className='min-h-screen flex flex-col items-center justify-center'>
-        
-        <h1 className='text-4xl font-extrabold'>Count : {count}</h1>
-        <h1 className='text-4xl font-extrabold'>Previous Count : {prevCount.current}</h1>
+
+        <input 
+        ref={inputRef}
+        onChange={(e) => {setName(e.target.value)}}
+        value={name}
+        className='border border-white rounded px-4 py-2'
+        placeholder='Type something...'
+        type="text" />
 
         <button 
-        onClick={() => {setCount(count + 1)}}
-        className='border border-white px-3 py-2 mt-5 rounded cursor-pointer hover:bg-white hover:text-black'
-        >Increment</button>
+        onClick={handleReset}
+        className='border border-white px-4 py-2 mt-5 rounded bg-gray-800 active:scale-50'>Reset</button>
+
+        <button 
+        onClick={handleChange}
+        className='border border-white px-4 py-2 mt-5 rounded bg-gray-800 active:scale-50'>Change Color</button>
 
       </div>
     </>
